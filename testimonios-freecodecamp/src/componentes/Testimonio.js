82d50db6752aa360d26f71cbacb801f9301b9656
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2024-06-21 20:56:18 -0600

    generando proyecto

---
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Testimonio() {
+    return(
+      <div className="contenedor-testimonio">
+        <img className="imagen-testimonio" src={require("../assets/test1.png")} alt="foto1"/>
+      </div>
+    );
+}
