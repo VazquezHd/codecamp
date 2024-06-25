048f7af701bc74155515efbe194675d0af9499a2
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2024-06-24 20:19:37 -0600

    agregando estilos a testimonio

--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,9 +1,18 @@
 import React from "react";
+import '../stylesheets/Testimonio.css'; 
 
 function Testimonio() {
     return(
       <div className="contenedor-testimonio">
-        <img className="imagen-testimonio" src={require("../assets/test1.png")} alt="foto1"/>
+        <img className="imagen-testimonio" src={require("../assets/test3.png")} alt="foto3"/>
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio">Emma Bostian en Suecia</p>
+          <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
+          <p>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+        </div>
       </div>
+      
     );
 }
+
+export default Testimonio;
